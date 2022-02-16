af4dfdd489ec4ebccb1aae10c029d4be5161fce4
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-16 13:08:05 +0200

    move stuff to utils/optional.sh

--- setup_debian.sh
+++ setup_debian.sh
@@ -91,46 +91,8 @@ rm google-chrome-stable_current_amd64.deb
 sudo apt install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Microsoft Fonts Install
-while true; do
-    read -p "Do you wish to install microsoft fonts? " yn
-    case $yn in
-        [Yy]* ) ./utils/microsoft-fonts-install.sh; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# Snap Remove
-while true; do
-    read -p "Do you wish to remove the snap package manager? " yn
-    case $yn in
-        [Yy]* ) ./utils/snap-nuke.sh; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-## Install custom run_once script
-while true; do
-    read -p "Do you want to install bash aliases? " yn
-    case $yn in
-        [Yy]* ) ./utils/bash_aliases.sh; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-#install wiims iso and szs tools
-while true; do
-    read -p "Do you want to install wiimms iso and szs tools? " yn
-    case $yn in
-        [Yy]* ) ./utils/wiim_install.sh; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
+##install optional stuff
+./utils/optional.sh
 
 cat << EOF
 ┌─────────────────────────────────────────────────────────────────────┐
