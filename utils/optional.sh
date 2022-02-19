5dd4b87182156d84dc2315e75d6c09e780a992a7
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-19 18:25:46 +0200

    add steam

--- utils/optional.sh
+++ utils/optional.sh
@@ -6,7 +6,7 @@ chmod u+x ./utils/wiim_install.sh
 chmod u+x ./utils/noto-color-emoji.sh
 # Microsoft Fonts Install
 while true; do
-    read -p "Do you wish to install microsoft fonts? " yn
+    read -p "Do you wish to install microsoft fonts? [Y/n]:" yn
     case $yn in
         [Yy]* ) ./utils/microsoft-fonts-install.sh; break;;
         [Nn]* ) break;;
@@ -16,7 +16,7 @@ done
 
 # Snap Remove
 while true; do
-    read -p "Do you wish to remove the snap package manager? " yn
+    read -p "Do you wish to remove the snap package manager? [Y/n]:" yn
     case $yn in
         [Yy]* ) ./utils/snap-nuke.sh; break;;
         [Nn]* ) break;;
@@ -26,7 +26,7 @@ done
 
 ## Install custom run_once script
 while true; do
-    read -p "Do you want to install bash aliases? " yn
+    read -p "Do you want to install bash aliases? [Y/n]:" yn
     case $yn in
         [Yy]* ) ./utils/bash_aliases.sh; break;;
         [Nn]* ) break;;
@@ -36,7 +36,7 @@ done
 
 #install wiims iso and szs tools
 while true; do
-    read -p "Do you want to install wiimms iso and szs tools? " yn
+    read -p "Do you want to install wiimms iso and szs tools? [Y/n]:" yn
     case $yn in
         [Yy]* ) ./utils/wiim_install.sh; break;;
         [Nn]* ) break;;
@@ -46,9 +46,19 @@ done
 
 #install latest noto-color-emoji.ttf
 while true; do
-    read -p "Do you want to install Noto color emoji font? " yn
+    read -p "Do you want to install Noto color emoji font? [Y/n]:" yn
     case $yn in
         [Yy]* ) ./utils/noto-color-emoji.sh; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no";;
+    esac
+done
+
+#install steam
+while true; do
+    read -p "Do you want to install Steam? [Y/n]:" yn
+    case $yn in
+        [Yy]* ) ./utils/steam.sh; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no";;
     esac
