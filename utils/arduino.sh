75b5ced0c567127a602c800775daec38bda5d8f0
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-20 17:33:28 +0200

    Update arduino.sh
    move install directory to /usr/share/aleksireede

--- utils/arduino.sh
+++ utils/arduino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo mkdir /usr/share/arduino
-cd /usr/share/arduino
+mkdir /usr/share/aleksireede
+cd /usr/share/aleksireede
 sudo wget https://downloads.arduino.cc/arduino-nightly-linux64.tar.xz
 sudo tar -xf arduino-nightly-linux64.tar.xz
 sudo rm arduino-nightly-linux64.tar.xz
