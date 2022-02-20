68cd2ac49a00a9c9ff93016aa87f6a861ba8784d
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-20 18:27:07 +0200

    Update microsoft-fonts-install.sh
    more readability

--- utils/microsoft-fonts-install.sh
+++ utils/microsoft-fonts-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ -f /etc/arch-release ]
 then
-#Only for ArchLinux
-yay -Suy ttf-ms-fonts
+    #Only for ArchLinux
+    yay -Suy ttf-ms-fonts
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ] || [ -f /etc/linuxmint/info ]
 then
-#Only for Ubuntu/Mint/Debian
-sudo apt update && sudo apt install ttf-mscorefonts-installer
+    #Only for Ubuntu/Mint/Debian
+    sudo apt update && sudo apt install --assume-yes -y ttf-mscorefonts-installer
 else
-echo ""
-echo "Your system is not supported!"
-echo ""
+    echo ""
+    echo "Your system is not supported!"
+    echo ""
 fi
