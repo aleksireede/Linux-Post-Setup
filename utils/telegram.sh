c6110273f778165ce7770efecb60b4b2fe2fd064
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-20 17:35:45 +0200

    Update telegram.sh
    modify installpath

--- utils/telegram.sh
+++ utils/telegram.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Install telegram
+InstallPath="/usr/share/aleksireede/Telegram"
 wget -O telegram.tar.xz https://telegram.org/dl/desktop/linux
 tar -xf telegram.tar.xz
 rm telegram.tar.xz
 cd Telegram
-sudo mv Telegram /usr/share/
-sudo mv Updater /usr/share/
-/usr/share/Updater
+mkdir $InstallPath
+sudo mv Telegram $InstallPath
+sudo mv Updater $InstallPath
+$InstallPath/Updater
