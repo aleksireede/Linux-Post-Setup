e8519a3f5376f2b171748b97c85b0d5e263e0078
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-20 17:36:18 +0200

    Update arduino.sh
    fix typo

--- utils/arduino.sh
+++ utils/arduino.sh
@@ -4,6 +4,6 @@ cd /usr/share/aleksireede
 sudo wget https://downloads.arduino.cc/arduino-nightly-linux64.tar.xz
 sudo tar -xf arduino-nightly-linux64.tar.xz
 sudo rm arduino-nightly-linux64.tar.xz
-cd arduino-nightly
+cd ./arduino-nightly
 ./arduino-linux-setup.sh $USER
 sudo ./install.sh
