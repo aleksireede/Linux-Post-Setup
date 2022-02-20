47f94ed8fd8bc73ea8e6a73335e4cd5375df3cfe
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-20 18:28:55 +0200

    Create Installer.sh
    create one file for both arch and debian kind of laucnher for the actual scripts

---
+++ Installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -f /etc/arch-release ]
+then
+    #Only for ArchLinux
+    chmod u+x ./setup_arch.sh
+    ./setup_arch.sh
+elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ] || [ -f /etc/linuxmint/info ]
+then
+    #Only for Ubuntu/Mint/Debian
+    chmod u+x ./setup_debian.sh
+    ./setup_debian.sh
+else
+    echo ""
+    echo "Your system is not supported!"
+    echo ""
+fi
