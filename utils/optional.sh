bb92c8c955bafc30a5ebf61b9e46f81acb9e68ad
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-21 15:45:58 +0200

    Update optional.sh
    fixed wrong function declaration

--- utils/optional.sh
+++ utils/optional.sh
@@ -8,22 +8,22 @@ chmod u+x ./utils/arduino.sh
 chmod u+x ./utils/telegram.sh
 chmod u+x ./utils/grub.sh
 
-utility("Do you want to install microsoft fonts?","./utils/microsoft-fonts-install.sh")# Microsoft Fonts Install
-utility("Do you wish to remove the snap package manager?","./utils/snap-nuke.sh")# Snap Remove
-utility("Do you want to install bash aliases?","./utils/bash_aliases.sh")# Install bash aliases
-utility("Do you want to install wiimms iso and szs tools?","./utils/wiim_install.sh")#install wiims iso and szs tools
-utility("Do you want to install Noto color emoji font?","./utils/noto-color-emoji.sh")#install latest noto-color-emoji.ttf
-utility("Do you want to install Telegram?","./utils/telegram.sh")#install telegram
-utility("Do you want to install Arduino?","./utils/arduino.sh")#install arduino
-utility("Do you want to make changes to grub?","./utils/grub.sh")#install grub fixes
-
-function utility($echoline, $util){
+function utility(){
     while true; do
-        read -p "$echoline [Y/n]:" yn
+        read -p "$1 [Y/n]:" yn
         case $yn in
-            [Yy]* ) $util; break;;
+            [Yy]* ) $2; break;;
             [Nn]* ) break;;
             * ) echo "Please answer yes or no.";;
         esac
     done
 }
+
+utility "Do you want to install microsoft fonts?" ./utils/microsoft-fonts-install.sh # Microsoft Fonts Install
+utility "Do you wish to remove the snap package manager?" ./utils/snap-nuke.sh # Snap Remove
+utility "Do you want to install bash aliases?" ./utils/bash_aliases.sh # Install bash aliases
+utility "Do you want to install wiimms iso and szs tools?" ./utils/wiim_install.sh #install wiims iso and szs tools
+utility "Do you want to install Noto color emoji font?" ./utils/noto-color-emoji.sh #install latest noto-color-emoji.ttf
+utility "Do you want to install Telegram?" ./utils/telegram.sh #install telegram
+utility "Do you want to install Arduino?" ./utils/arduino.sh #install arduino
+utility "Do you want to make changes to grub?" ./utils/grub.sh #install grub fixes
