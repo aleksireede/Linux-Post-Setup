c0a513d845f335cf13c9f891f385c295b6690e99
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-21 16:04:01 +0200

    Update grub.sh

--- utils/grub.sh
+++ utils/grub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$grubfile="/etc/default/grub"
-$customfile="/etc/grub.d/40_custom"
-cat << EOF >> $grubfile
-GRUB_SAVEDEFAULT=true
+grubfile="/etc/default/grub"
+customfile="/etc/grub.d/40_custom"
+cat <<EOF>> $grubfile
+'GRUB_SAVEDEFAULT=true'
 EOF
 sudo sed -i "s/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/" $grubfile
 sudo sed -i "s/GRUB_TIMEOUT=0/GRUB_TIMEOUT=20/" $grubfile
@@ -12,27 +12,27 @@ sudo sed -i "s/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu/" $grubfile
 while true; do
     read -p "Do you want to add windows 11 entry to grub? [Y/n]:" yn
     case $yn in
-        [Yy]* ) grub_windows_entry; break;;
+        [Yy]* ) grubwindowsentry; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-grub_windows_entry () {
-    cat << EOF >> $grubfile
-    GRUB_DISABLE_OS_PROBER=true
-    EOF
-    cat << EOF >> $customfile
-    menuentry 'Windows 11 (kohteella /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'windows-efi-7C91-63A3' {
-	    savedefault
-	    insmod part_gpt
-	    insmod fat
-	    if [ x$feature_platform_search_hint = xy ]; then
-	    search --no-floppy --fs-uuid --set=root  7C91-63A3
-	    else
-	    search --no-floppy --fs-uuid --set=root 7C91-63A3
-	    fi
-	    chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-    }
-    EOF
+function grubwindowsentry(){
+cat <<EOF>> $grubfile
+GRUB_DISABLE_OS_PROBER=true
+EOF
+cat <<EOF>> $customfile
+'menuentry 'Windows 11 (kohteella /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'windows-efi-7C91-63A3' {'
+'    savedefault'
+'    insmod part_gpt'
+'    insmod fat'
+'    if [ x$feature_platform_search_hint = xy ]; then'
+'    search --no-floppy --fs-uuid --set=root  7C91-63A3'
+'    else'
+'    search --no-floppy --fs-uuid --set=root 7C91-63A3'
+'    fi'
+'    chainloader /EFI/Microsoft/Boot/bootmgfw.efi'
+'}'
+EOF
 }
