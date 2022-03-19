1ffbb429d96c2a9037624cbca3e4e91e6fd6a332
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-03-19 21:39:16 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Install Discord as appimage
+curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
+zap install --github --from=srevinsaju/discord-appImage discord-appimage
+zap install --github --from=srevinsaju/Telegram-AppImage telegram-appimage
 if [ -f /etc/arch-release ]
 then
     #Only for ArchLinux
