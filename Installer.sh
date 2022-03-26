912fdb4eafdd3646814abb173d54017e52b46163
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-03-26 18:24:55 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,6 +3,12 @@
 curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
 zap install --github --from=srevinsaju/discord-appImage discord-appimage
 zap install --github --from=srevinsaju/Telegram-AppImage telegram-appimage
+curl -sSL https://install.python-poetry.org | python3 -
+git clone https://github.com/srevinsaju/youtube-music-dl
+cd youtube-music-dl
+poetry install
+poetry run cli --help
+
 if [ -f /etc/arch-release ]
 then
     #Only for ArchLinux
