8d2d7586596be5b37c8fb5c6ee255c19e80b46b6
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-03-30 15:53:15 +0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# Install Discord as appimage
-curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
-zap install --github --from=srevinsaju/discord-appImage discord-appimage
-zap install --github --from=srevinsaju/Telegram-AppImage telegram-appimage
-curl -sSL https://install.python-poetry.org | python3 -
-git clone https://github.com/srevinsaju/youtube-music-dl
-cd youtube-music-dl
-poetry install
-poetry run cli --help
 
 if [ -f /etc/arch-release ]
 then
