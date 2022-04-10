abaff97a8b84edfae26e62b8135e024bb7f4158e
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-10 20:16:22 +0300

    Update telegram.sh

--- utils/telegram.sh
+++ utils/telegram.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# Install telegram
-InstallPath="/usr/share/aleksireede/Telegram"
-wget -O telegram.tar.xz https://telegram.org/dl/desktop/linux
-tar -xf telegram.tar.xz
-rm telegram.tar.xz
-cd Telegram
-mkdir $InstallPath
-sudo mv Telegram $InstallPath
-sudo mv Updater $InstallPath
-$InstallPath/Updater
+function telegram-native(){
+    InstallPath="/usr/share/aleksireede/Telegram"
+    wget -O telegram.tar.xz https://telegram.org/dl/desktop/linux
+    tar -xf telegram.tar.xz
+    rm telegram.tar.xz
+    cd Telegram
+    mkdir $InstallPath
+    sudo mv Telegram $InstallPath
+    sudo mv Updater $InstallPath
+    $InstallPath/Updater
+    }
+while true; do
+    read -p "Do you want to install telegram as native or flatpak? [N/f]" yn
+    case $yn in
+        [Nn]* ) telegram-native; break;;
+        [Ff]* ) sudo flatpak install org.telegram -y --system; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
