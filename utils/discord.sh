a807a388369eda1e8071a32c77b1ecb8d9fe3244
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-10 19:39:21 +0300

    Update discord.sh
    replace appimage with flatpak

--- utils/discord.sh
+++ utils/discord.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while true; do
-    read -p "Do you want to install discord as native or appimage? [N/a]" yn
+    read -p "Do you want to install discord as native or flatpak? [N/f]" yn
     case $yn in
-        [Aa]* ) xdg-open https://discord.com/api/download?platform=linux&format=deb; break;;
-        [Nn]* ) zap install --github --from=srevinsaju/discord-appImage discord-appimage; break;;
+        [Ff]* ) xdg-open https://discord.com/api/download?platform=linux&format=deb; break;;
+        [Nn]* ) sudo flatpak install Discord; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
