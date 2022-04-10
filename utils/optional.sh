9bc09c10cfa2145d7760708e7edef2be6e100111
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-10 20:15:05 +0300

    Update optional.sh

--- utils/optional.sh
+++ utils/optional.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-chmod u+x ./utils/microsoft-fonts-install.sh
-chmod u+x ./utils/snap-nuke.sh
-chmod u+x ./utils/bash_aliases.sh
-chmod u+x ./utils/wiim_install.sh
-chmod u+x ./utils/noto-color-emoji.sh
-chmod u+x ./utils/arduino.sh
-chmod u+x ./utils/telegram.sh
-chmod u+x ./utils/grub.sh
-chmod u+x ./utils/appimage_zap.sh
-chmod u+x ./discord.sh
+#loops through files with .sh ending and uses the below funtion "utility" to ask the user id they want to execute it
+readarray -d '' files < <(find . -type f \(-name "*.sh" ! -name "optional.sh" \)-print0)
+for i in ${!files[@]}; do
+  chmod u+x ${files[$i]}
+  utility "Do you want to execute ${files[$i]}" ${files[$i]}
+done
 
 function utility(){
     while true; do
@@ -20,14 +16,3 @@ function utility(){
         esac
     done
 }
-
-utility "Do you want to install microsoft fonts?" ./utils/microsoft-fonts-install.sh # Microsoft Fonts Install
-utility "Do you wish to remove the snap package manager?" ./utils/snap-nuke.sh # Snap Remove
-utility "Do you want to install bash aliases?" ./utils/bash_aliases.sh # Install bash aliases
-utility "Do you want to install wiimms iso and szs tools?" ./utils/wiim_install.sh #install wiims iso and szs tools
-utility "Do you want to install Noto color emoji font?" ./utils/noto-color-emoji.sh #install latest noto-color-emoji.ttf
-utility "Do you want to install Telegram?" ./utils/telegram.sh #install telegram
-utility "Do you want to install Arduino?" ./utils/arduino.sh #install arduino
-utility "Do you want to make changes to grub?" sudo ./utils/grub.sh #install grub fixes
-utility "Do you want to install discord?" ./utils/discord.sh
-utility "Do you want to install zap appimage manager?" ./utils/appimage_zap.sh
