db5e83f1e723536bf68591de241dfdf4e34158ae
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-16 16:21:58 +0300

    Create arch.py

---
+++ utils/arch.py
@@ -0,0 +1,30 @@
+# Importing Path from pathlib2 module
+from pathlib2 import Path
+  
+# Creating a function to
+# replace the text
+def replacetext(search_text, replace_text):
+  
+    # Opening the file using the Path function
+    file = Path(r"/etc/pacman.conf")
+  
+    # Reading and storing the content of the file in
+    # a data variable
+    data = file.read_text()
+  
+    # Replacing the text using the replace function
+    data = data.replace(search_text, replace_text)
+  
+    # Writing the replaced data
+    # in the text file
+    file.write_text(data)
+  
+    # Return "Text replaced" string
+    return "Text replaced"
+  
+# Calling the replacetext function
+# and printing the returned statement
+print(replacetext("#[multilib]", "[multilib]"))
+print(replacetext("#Include = /etc/pacman.d/mirrorlist", "Include = /etc/pacman.d/mirrorlist"))
+print(replacetext("#ParallelDownloads=5", "ParallelDownloads=5"))
+print(replacetext("#Color", "Color"))
