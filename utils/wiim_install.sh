77d9caf0bde6f3153e9045a8fa9d132868711efc
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-16 16:01:25 +0300

    Update wiim_install.sh

--- utils/wiim_install.sh
+++ utils/wiim_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-isofile = wit-v3.04a-r8427-x86_64
-szsfile = szs-v2.26a-r8462-x86_64
+export isofile=wit-v3.04a-r8427-x86_64
+export szsfile=szs-v2.26a-r8462-x86_64
 wget https://wit.wiimm.de/download/$isofile.tar.gz
 tar -xf $isofile.tar.gz
 cd $isofile
 chmod u+x ./install.sh
-./install.sh
+sudo ./install.sh
 cd ..
 rm -rf $isofile
 rm -rf $isofile.tar.gz
@@ -13,7 +13,7 @@ wget https://szs.wiimm.de/download/$szsfile.tar.gz
 tar -xf $szsfile.tar.gz
 cd $szsfile
 chmod u+x ./install.sh
-./install.sh
+sudo ./install.sh
 cd ..
 rm -rf $szsfile
 rm -rf $szsfile.tar.gz
