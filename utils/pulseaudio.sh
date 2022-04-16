50bfc86c2a0a850ff4288eb25bd000fd37eaa769
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-16 16:23:08 +0300

    Update pulseaudio.sh

--- utils/pulseaudio.sh
+++ utils/pulseaudio.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+if [ -f /etc/pulse/default.pa ]
+then
 sudo sed -i "s/load-module module-bluetooth-policy/load-module module-bluetooth-policy auto_switch=false/" /etc/pulse/default.pa
+fi
 #disable bluetooth hfp/hsp auto switch on ms teams
