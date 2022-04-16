efb17c8936259dbc5c7ef38e70a4c9c808b51449
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-16 15:59:03 +0300

    Update noto-color-emoji.sh

--- utils/noto-color-emoji.sh
+++ utils/noto-color-emoji.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 wget https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
+if [ -f /usr/share/fonts/truetype/ ]
+then
 sudo mv NotoColorEmoji.ttf /usr/share/fonts/truetype/
+fi
+if [ -f /usr/share/fonts/noto/ ]
+then
+sudo mv NotoColorEmoji.ttf /usr/share/fonts/noto/
+fi
