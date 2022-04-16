f675c54857943543341ccbb2b42d75052a2149a8
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-16 15:56:22 +0300

    Update appimage_zap.sh

--- utils/appimage_zap.sh
+++ utils/appimage_zap.sh
@@ -2,9 +2,3 @@
 curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | sudo bash -s
 zap install --github --from=srevinsaju/Telegram-AppImage telegram-appimage
 zap install --github --from=nextcloud/desktop
-
-curl -sSL https://install.python-poetry.org | python3 -
-git clone https://github.com/srevinsaju/youtube-music-dl
-cd youtube-music-dl
-poetry install
-poetry run cli --help
