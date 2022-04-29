eec0ce6b18692c3e0c528be842fde72f2d212988
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-29 14:42:14 +0300

    Create doas.sh

---
+++ debian/doas.sh
@@ -0,0 +1,10 @@
+#Install doas
+git clone https://github.com/slicer69/doas.git
+cd ./doas
+make
+sudo make install
+wget -O doas.conf https://pastebin.com/raw/EK6hud2S
+sudo chmod 644 ./doas.conf
+sudo cp ./doas.conf /usr/local/etc/doas.conf
+cd ..
+rm -rf ./doas
