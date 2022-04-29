7661f3f982bc78e9fcf4dbe6e81f6b13f6321217
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-04-29 14:32:37 +0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,17 +1,97 @@
 #!/bin/bash
+lolcatbin="/usr/games/lolcat"
+function execute(){
+    lolcat << EOF
+    ┌──────────────────────────────────────────────────────────────────────┐
+    │This Bash Script is made by u/Techno021 to nuke snaps from Ubuntu.    │
+    │                                                                      │
+    │Note: This has only been tested on Ubuntu 20.04 LTS, and may not work │
+    │as intended on other versions. I will not be responsible for any      │
+    │damage if this script breaks your pc                                  │
+    └──────────────────────────────────────────────────────────────────────┘
+EOF
+
+    while true; do
+        read -p "Do you wish to run the script? " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+function sudocheck(){
+    if [[ "$EUID" == 0 ]]
+    then 
+    lolcat << EOF
+    ┌──────────────────────────────────────────────────────────────────────┐
+    │Please don't run this script as root as it may break you system.      │
+    │We will ask you for the password if we need root access.              │
+    └──────────────────────────────────────────────────────────────────────┘
+    ┬─┬ ノ( ゜-゜ノ)
+EOF
+    exit
+    fi
+}
+
+# Download additional scripts from other sources
+cd ./utils/sh
+wget https://raw.githubusercontent.com/MasterGeekMX/snap-to-flatpak/main/snap-to-flatpak.sh
 
 if [ -f /etc/arch-release ]
 then
     #Only for ArchLinux
-    chmod u+x ./setup_arch.sh
-    ./setup_arch.sh
+    # install lolcat
+    if [ -f "$lolcatbin" ]; then
+        echo "$lolcatbin already exists."
+        echo "You don't want to install it twice"
+    else
+        sudo pacman -S --noconfirm lolcat
+    fi#end lolcat
+    sudocheck
+    execute
+    chmod u+x ./arch/Packages.sh
+    ./arch/Packages.sh
+    python ./utils/arch.py
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ] || [ -f /etc/linuxmint/info ]
 then
     #Only for Ubuntu/Mint/Debian
-    chmod u+x ./setup_debian.sh
-    ./setup_debian.sh
+    #install lolcat if not found
+    if [ -f "$lolcatbin" ]; then
+        echo "$lolcatbin already exists."
+        echo "You don't want to install it twice"
+    else 
+	    cd ~
+	    ## install lolcat
+	    sudo apt --assume-yes -y install rubygems git
+	    git clone https://github.com/aleksireede/lolcat.git
+	    cd lolcat/bin
+	    sudo gem install lolcat
+	    sudo mv lolcat $lolcatbin
+	    cd ~
+	    rm -rf ./lolcat
+    fi#end lolcat
+    sudocheck
+    execute
+    chmod u+x ./debian/Packages.sh
+    ./debian/Packages.sh
+    python ./utils/pulseaudio.py
 else
     echo ""
     echo "Your system is not supported!"
     echo ""
+    exit
 fi
+##install all optonal things
+chmod u+x ./utils/optional.sh
+./utils/optional.sh
+
+lolcat << EOF
+┌─────────────────────────────────────────────────────────────────────┐
+│All Done! This script has succesfully completed, please reboot so    │
+│that changes take effect.                                            │
+└─────────────────────────────────────────────────────────────────────┘
+(╯°□°）╯︵ ┴─┴ 
+EOF
+echo
