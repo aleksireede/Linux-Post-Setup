8eb6bedcf21d966eec997b252260eb3c23502517
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-05-17 16:30:30 +0300

    Update Packages.sh

--- debian/Packages.sh
+++ debian/Packages.sh
@@ -18,7 +18,7 @@ sudo apt --assume-yes -y install \
     libpulse-java vlc libreoffice gimp \
     build-essential make bison flex libpam0g-dev \
     keepassxc xdg-utils jq flatpak \
-    vim mpv
+    vim mpv audacious
 
 # Install steam
 wget -O steam.deb https://cdn.akamai.steamstatic.com/client/installer/steam.deb
