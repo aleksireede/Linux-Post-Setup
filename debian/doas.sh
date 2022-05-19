1431a65b7c867737ee8ae57c562d8b5a17fcb91f
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-05-19 19:36:16 +0300

    fixed chmod

--- debian/doas.sh
+++ debian/doas.sh
@@ -4,7 +4,7 @@ cd ./doas
 make
 sudo make install
 wget -O doas.conf https://pastebin.com/raw/EK6hud2S
-sudo chmod 644 ./doas.conf
+sudo chmod 600 ./doas.conf
 sudo cp ./doas.conf /usr/local/etc/doas.conf
 cd ..
 rm -rf ./doas
