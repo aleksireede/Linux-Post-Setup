64c5f851e0a49f3103611ef79f04daa2ac7d0443
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-06-06 18:41:06 +0300

    Update Packages.sh

--- debian/Packages.sh
+++ debian/Packages.sh
@@ -23,7 +23,7 @@ sudo apt --assume-yes -y install \
     keepassxc xdg-utils jq flatpak \
     vim mpv audacious htop inkscape \
     kate kdenlive nemo-nextcloud nemo \
-    synaptic gdebi itstool
+    synaptic gdebi itstool libtbb2-dev
 
 # Install steam
 wget -O steam.deb https://cdn.akamai.steamstatic.com/client/installer/steam.deb
