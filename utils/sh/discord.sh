9e321f9ac3e9533ba44216a1ee4f36a3aa7b4e6d
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-06-06 21:18:19 +0300

    Update discord.sh

--- utils/sh/discord.sh
+++ utils/sh/discord.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-if [ -f /etc/arch-release ]
-then
-    #Only for ArchLinux
-    sudo flatpak install Discord
-elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ] || [ -f /etc/linuxmint/info ]
-then
-    #Only for Ubuntu/Mint/Debian
-    xdg-open https://discord.com/api/download?platform=linux&format=deb
-else
-    echo ""
-    echo "Your system is not supported!"
-    echo ""
-fi
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+while true; do
+    read -p "Do you want to install discord as deb or flatpak? [N/f]" yn
+    case $yn in
+        [Nn]* ) xdg-open https://discord.com/api/download?platform=linux&format=deb; break;;
+        [Ff]* ) sudo flatpak install com.discordapp.Discord -y --system; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
