c848544842ec9455b98f2f2220ed863acc0112f7
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-12 19:14:21 +0300

    Update doas.sh

--- debian/doas.sh
+++ debian/doas.sh
@@ -5,8 +5,8 @@ make
 sudo make install
 wget -O doas.conf https://pastebin.com/raw/EK6hud2S
 sudo chmod 600 ./doas.conf
-chown root:root ./doas.conf
-dos2unix ./doas.conf
+sudo chown root:root ./doas.conf
+sudo dos2unix ./doas.conf
 sudo cp ./doas.conf /usr/local/etc/doas.conf
 cd ..
 rm -rf ./doas
