b4fe297db08f45edbc992f00c461edd8ed2e27bd
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-14 20:26:48 +0300

    Update discord.sh

--- utils/sh/discord.sh
+++ utils/sh/discord.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 while true; do
-    read -p "Do you want to install discord as deb or flatpak? [N/f]" yn
+    read -p "Do you want to install discord as debian or arch or flatpak? [D/f/a]" yn
     case $yn in
-        [Nn]* ) xdg-open https://discord.com/api/download?platform=linux&format=deb; break;;
+        [Dd]* ) xdg-open https://discord.com/api/download?platform=linux&format=deb; break;;
         [Ff]* ) sudo flatpak install com.discordapp.Discord -y --system; break;;
-        * ) echo "Please answer yes or no.";;
+        [Aa]* ) sudo pacman -S discord; break;;
+        * ) echo "Please answer.";;
     esac
 done
