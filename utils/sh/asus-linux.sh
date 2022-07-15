7337ee1d9f781ef52eef3c71aa2ea6d3fdf2f020
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-15 14:30:40 +0300

    Update asus-linux.sh

--- utils/sh/asus-linux.sh
+++ utils/sh/asus-linux.sh
@@ -2,9 +2,10 @@
 if [ -f /etc/arch-release ]
 then
     #Only for ArchLinux
-    echo ""
-    echo "Your system is not supported!"
-    echo ""
+    git clone https://gitlab.com/asus-linux/asusctl.git
+    cd asusctl
+    make
+    sudo make install
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ] || [ -f /etc/linuxmint/info ]
 then
     #Only for Ubuntu/Mint/Debian
