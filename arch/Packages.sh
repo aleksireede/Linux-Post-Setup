64b08f04ab2261dfad2b8b816773140af8f3f0f6
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-16 19:52:02 +0300

    Update Packages.sh

--- arch/Packages.sh
+++ arch/Packages.sh
@@ -59,12 +59,8 @@ paru -Suy --needed --quiet \
     gnome-console woeusb-ng \
     syncthingtray-qt6 boost-libs-git \
     c++utilities qtforkawesome-qt6 \
-    qtutilities-qt6 easyeffects libadwaita \
-    libbs2b fmt lilv rubberband libsamplerate \
-    libsigc++-3.0 libsndfile mda.lv2 zam-plugins \
-    calf gtk4 lsp-plugins pipewire-pulse rnnoise \
-    tbb zita-convolver appstream-glib libebur128 \
-    gnome-todo
+    qtutilities-qt6 gnome-todo
+flatpak install easyeffects
 
 #doas    (sudo replacement)[better]{more secure}
 if ! [ -f /etc/doas.conf ]
