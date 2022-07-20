a33ff256415ef45bb87189f9da6e7da6dd35916e
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-20 20:41:41 +0300

    Update python.sh

--- utils/sh/python.sh
+++ utils/sh/python.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python3 -m pip install yt-dlp fetchcord pathlib2 telegram-send
 sudo python3 -m pip install yt-dlp fetchcord pathlib2 telegram-send
+python3 -m pip install python-telegram-bot -U --pre
+sudo python3 -m pip install python-telegram-bot -U --pre
