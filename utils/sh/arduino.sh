87a2081d98a154b93696c2713ab42949bfa14895
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-07-21 20:52:44 +0300

    Update arduino.sh

--- utils/sh/arduino.sh
+++ utils/sh/arduino.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 readarray -d '' cwd < <(pwd)
-sudo mkdir /usr/share/aleksireede
+if [ ! -f /usr/share/aleksireede ]
+then
+    sudo mkdir /usr/share/aleksireede
+fi
 cd /usr/share/aleksireede
+if [ -f ./arduino-nightly ]
+then
+    sudo rm -rf ./arduino-nightly
+fi
 sudo wget https://downloads.arduino.cc/arduino-nightly-linux64.tar.xz
 sudo tar -xf arduino-nightly-linux64.tar.xz
 sudo rm arduino-nightly-linux64.tar.xz 
