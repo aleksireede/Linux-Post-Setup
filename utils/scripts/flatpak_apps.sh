08b871b8fd2adaa9d45bf2fb5f781901bb41e8c7
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-09-05 21:46:38 +0300

    Create flatpak_apps.sh

---
+++ utils/scripts/flatpak_apps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+flatpak install flathub com.lunarclient.LunarClient #lunar client
+flatpak install flathub org.telegram.desktop        #telegram
+flatpak install flathub com.discordapp.Discord      #discord
+flatpak install flathub org.DolphinEmu.dolphin-emu  #dolphin emulator
