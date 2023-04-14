5879e1fd2ec6ed8d5fb42c763b5d4daa51d043bc
Aleksi Reede <aleksi.reede@gmail.com>
2023-04-14 22:03:50 +0300

    remove invalid packages

--- Installer.py
+++ Installer.py
@@ -2,16 +2,11 @@
 import Program_Main
 import subprocess
 import os
-import pathlib2
-import git
-import shutil
 from program_commands import get_username, os_check, is_tool
 
 username = get_username()
 os.environ["PATH"] += ":/home/"+username+"/.local/bin/:/home/" + \
-    username+"/.local/share/gem/ruby/3.0.0/bin/"
-lolcat_binary_path = pathlib2.Path("/home", username, ".local/bin/lolcat")
-lolcat_temp_path = pathlib2.Path("/tmp/lolcat")
+    ":/home/"+username+"/.local/share/gem/ruby/3.0.0/bin/"
 system = os_check()
 
 
@@ -24,13 +19,5 @@ if not is_tool("lolcat") and not is_tool("gem"):
 
 
 if not is_tool("lolcat"):
-    git.Repo.clone_from(
-        "https://github.com/aleksireede/lolcat.git", lolcat_temp_path)
-    subprocess.run(["sudo", "gem", "install", "lolcat"],
-                   cwd=pathlib2.Path(lolcat_temp_path, "bin"), check=True, text=True)
-    shutil.copyfile(pathlib2.Path(lolcat_temp_path,
-                    "bin", "lolcat"), lolcat_binary_path)
-    subprocess.run(["sudo", "chmod", "+x", lolcat_binary_path],
-                   check=True, text=True)
-    shutil.rmtree(lolcat_temp_path)
+    subprocess.run(["gem", "install", "lolcat"], check=True, text=True)
 Program_Main.main()
